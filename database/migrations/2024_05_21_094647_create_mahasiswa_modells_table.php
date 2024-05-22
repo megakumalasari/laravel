a472ba951433c0f92e04055a7fa9023c0cf19265
megakumalasari <megakumalasari8@gmail.com>
2024-05-22 15:49:30 +0700

    first commit

---
+++ database/migrations/2024_05_21_094647_create_mahasiswa_modells_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('mahasiswa', function (Blueprint $table) {
+            $table->id();
+            $table->string('npm',11)->unique();
+            $table->string('nama_mhs',25)->nullable();
+            $table->text('alamat');
+            $table->date('tgl_lahir')->nullable();
+            $table->enum('jk',['L','P']);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('mahasiswa');
+    }
+};
